76f4adb0f463d59fc902b2ce90a1ca4cbd9fcb7d
1243557 <steli3117@gmail.com>
2026-02-16 18:11:14 +0200

    database setup + initial entity

--- src/main/java/com/internship/stocks_api/models/CompanyInfo.java
+++ src/main/java/com/internship/stocks_api/models/CompanyInfo.java
@@ -1,4 +1,46 @@
 package com.internship.stocks_api.models;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "companies")
 public class CompanyInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String symbol;
+
+    @Column(nullable = true)
+    private String website;
+
+    @Column(nullable = true)
+    private String email;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
 }
