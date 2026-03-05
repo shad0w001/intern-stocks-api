7cf9a8206e053583ea5bf27c57211df2cecdf7d6
1243557 <steli3117@gmail.com>
2026-03-05 11:36:01 +0200

    implemented liquibase migrations

--- src/main/java/com/internship/stocks_api/StocksApiApplication.java
+++ src/main/java/com/internship/stocks_api/StocksApiApplication.java
@@ -1,15 +1,18 @@
 package com.internship.stocks_api;
 
+import me.paulschwarz.springdotenv.DotenvConfig;
+import me.paulschwarz.springdotenv.DotenvPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableFeignClients
 public class StocksApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StocksApiApplication.class, args);
+        SpringApplication.run(StocksApiApplication.class, args);
 	}
 
 }
